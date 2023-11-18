aa1c44aff57af1c8970139aaa64fa1d111208a65
camiladiaspinto <62036640+camiladiaspinto@users.noreply.github.com>
2023-11-18 12:09:11 +0000

    implementação do protocolo para get e response

--- gestor.py
+++ gestor.py
@@ -1,89 +1,98 @@
 import socket
 import sys
-import random
-import json
+import os
+import time
+
 ip = '127.0.0.1'
 port = 1234
-
+filename = 'last_P.txt'
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Obtém os valores da PDU a partir dos argumentos da linha de comando e converte para inteiros para ser mais fácil tratar dos dados
-S = 0 #mecanismo de segurança que é zero 
+S = 0  # mecanismo de segurança que é zero
 NS = 0
-Q=0
-P = random.randint(1,100)  #identificação do pedido . vai ter de ser mduado para ficheiro 
-Y = int(sys.argv[1]) #identidicaçaõ da primitiva 
+Q = 0
+Y = int(sys.argv[1])  # identificação da primitiva
+V = 10
 
-#Falta isot: não é permitido que durante V segundos o gestor identifique outro pedido com o mesmo I-ID, duvida 
-NW = int(sys.argv[2]) #quantidade de pares 
-NR = int(sys.argv[3])  #quantidade de pares de erros 
+# verifica se o arquivo existe e lê o valor nele, senão cria e começa do zero o p e o timestamp (v segundos)
+if os.path.exists(filename):
+    with open(filename, 'r') as file:
+        P, last_P_timestamp = map(float, file.read().split())
+else:
+    P = 0
+    last_P_timestamp = 0
+    #se nao existir, cria
+    with open(filename, 'w') as file:
+        file.write(f"{P} {last_P_timestamp}")
+
+N = int(sys.argv[2])  # quantidade de pares
+NR = int(sys.argv[3])  # quantidade de pares de erros
 
 W = []  # Lista de pares
-R=[] #Lista de erros 
+R = []  # Lista de erros
 
-if int(Y) == 2:
-    #começa na posicção 4 e vai desde essa posição até ao fim da quantidade de pares
-    for i in range(4, 4 + NW):
-        pair = sys.argv[i].split(',')
-        if len(pair) != 2:
-            print("Erro: Formato inválido para o par (I-ID, H ou N).")
-            sys.exit(1)
-        instancia = str(pair[0])
-        valor = str(pair[1])
-        W.append((instancia, valor))
-    #começa na quantidade final da lista pares e vai até ao final da lista erros 
-    for i in range(4+NW, 4+NW+ NR): 
-        pair = sys.argv[i].split(',')
-        if len(pair) != 2:
-                print("Erro: Formato inválido para o par (I-ID, E).")
-                sys.exit(1)
-        instancia = str(pair[0])
-        erro = str(pair[1])
-        R.append((instancia, erro))
-        
-else:
-    print("Primitiva não suportada.")
-    sys.exit(1)
+#tratamento de dados
+# começa na posição 4 e vai desde essa posição até ao fim da quantidade de pares
+for i in range(4, 4 + N):
+    pair = sys.argv[i].split(',')
+    if len(pair) != 2:
+        print("Erro: Formato inválido para o par (I-ID, H ou N).")
+        sys.exit(1)
+    instancia = str(pair[0])
+    valor = str(pair[1])
+    W.append((instancia, valor))
 
-s_data=str(S) + '\0'
-ns_data=str(NS) + '\0'
-q_data=str(Q) +'\0'
-y_data=str(Y) + '\0'
-p_data=str(P) + '\0'
-nw_data=str(NW) + '\0'
-nr_data=str(NR) + '\0'
+# começa na quantidade final da lista pares e vai até ao final da lista erros
+for i in range(4 + N, 4 + N + NR):
+    pair = sys.argv[i].split(',')
+    if len(pair) != 2:
+        print("Erro: Formato inválido para o par (I-ID, E).")
+        sys.exit(1)
+    instancia = str(pair[0])
+    erro = str(pair[1])
+    R.append((instancia, erro))
 
-#forma uma tupla de dados e depois transforma em bytes 
+current_time = time.time()
+if P == float(open(filename).read().split()[0]):
+    # Verifica se o intervalo de tempo entre os pedidos é menor que V segundos
+    if current_time - last_P_timestamp < V:
+        print(f"Erro: Não é permitido identificar outro pedido com o mesmo I-ID durante {V} segundos.")
+        sys.exit(1)
+
+s_data = str(S) + '\0'
+ns_data = str(NS) + '\0'
+q_data = str(Q) + '\0'
+y_data = str(Y) + '\0'
+
+last_P_timestamp = current_time
+
+# Incrementa o valor de P para o próximo pedido e escreve no ficheiro, com o timestamp
+P += 1
+with open(filename, 'w') as file:
+    file.write(f"{P} {last_P_timestamp}")
+
+p_data = str(P) + '\0'
+n_data = str(N) + '\0'
+nr_data = str(NR) + '\0'
+
+# forma uma tupla de dados e depois transforma em bytes
 W_str = ';'.join([f'{pair[0]},{pair[1]}' for pair in W])
 R_str = ';'.join([f'{pair[0]},{pair[1]}' for pair in R])
 W_bytes = W_str.encode('utf-8')
 R_bytes = R_str.encode('utf-8')
 
-#concatena as strings e tranforma em bytes 
-
-x_data='\0'
-bytes= s_data.encode('utf-8')+ns_data.encode('utf-8')+q_data.encode('utf-8')+y_data.encode('utf-8')+p_data.encode('utf-8')+nw_data.encode('utf-8')+nr_data.encode('utf-8')+W_bytes+x_data.encode('utf-8')+R_bytes
+# concatena as strings e transforma em bytes
+x_data = '\0'
+bytes = s_data.encode('utf-8') + ns_data.encode('utf-8') + q_data.encode('utf-8') + y_data.encode(
+    'utf-8') + p_data.encode('utf-8') + n_data.encode('utf-8') + nr_data.encode('utf-8') + W_bytes + x_data.encode(
+    'utf-8') + R_bytes
 print(bytes)
-
-
 
 server_address = (ip, port)
 client_socket.sendto(bytes, server_address)
 response, _ = client_socket.recvfrom(4096)
 print('Received response from server:', response)
-#num_requests = 1 
-
-#for _ in range(num_requests):
- #   client_socket.sendto(bytes, server_address)
-    #response, _ = client_socket.recvfrom(4096)
-    #print('Received response from server:', response)
 
 client_socket.close()
-
-#Problema: 
-#Resolver situação do id do pedido: deixa de ser random e gajo arranca verifica se tem um fichero com o ultimo P, se nao tiver faz um random e guarda lá e dpois para fazer o pediro vai ao ficheiro, lê um id e incrementa 
-
-#Trabalho futur(EDU):
-#Constuir a MIB
-#Tratar de guardar o key e a key id 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from matriz import MatrixZ
 import socket
 import threading
 import pickle
+import os
 
 #funcao que lê as configurações no ficheiro 
 def ReadConfigFile():
@@ -37,29 +38,36 @@ def StartMatrixUpdate(t, matriz):
         time.sleep(t)  # Aguarda o intervalo de atualização T (em ms)
 
 
+def handle_set_primitiva(parsed_data):
+    NW = str(parsed_data[5])
+    W_data = parsed_data[7].split(';')
+        #volta a formar uma tupla de pares 
+    W = [(pair.split(',')[0], pair.split(',')[1]) for pair in W_data]
 
-#função que inicia o server udp, udp comm
-def StartUDPServer(port, ip, matriz):
-    localIP = ip
-    localPort = port
-    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    UDPServerSocket.bind((localIP, localPort))
-    print("UDP listening")
+def handle_get_primitiva(parsed_data):
+    NL = str(parsed_data[5])
+    print(NL)
+    L_data = parsed_data[7].split(';')
+        #volta a formar uma tupla de pares 
+    L = [(pair.split(',')[0], pair.split(',')[1]) for pair in L_data]
+    print(L)
 
-    while True:
-        data, client_address = UDPServerSocket.recvfrom(4096)
-        #client_thread = threading.Thread(target=handle_client_request, args=(data, client_address, matriz, UDPServerSocket))
-        #client_thread.start()
-        # https://cppsecrets.com/users/110711510497115104971101075756514864103109971051084699111109/Python-UDP-Server-with-Multiple-Clients.php
-        # codigo não funcional, é só um esboço
-        # c_thread = threading.Thread(target = self.handle_request, args = (data, client_address, matrizZ, MIB))
-        # c_thread.daemon = True
-        # c_thread.start()
-        # a função handle_request terá todo o código para tratar dos pedidos
-           
-        # Obter o IP e a porta de origem do cliente
+def handle_response_primitiva(parsed_data):
+    NW = str(parsed_data[5])
+    W_data = parsed_data[7].split(';')
+        #volta a formar uma tupla de pares de erros
+    W = [(pair.split(',')[0], pair.split(',')[1]) for pair in W_data]
+    NR = str(parsed_data[6])
+    print(NR)
+    R_data = parsed_data[8].split(';')
+    R = [(pair.split(',')[0], pair.split(',')[1]) for pair in R_data]
+    print(R)
+
+
+def handle_client(data,client_address,matriz, UDPServerSocket):
+        print(f"Thread para cliente {client_address} iniciada.")
         client_ip, client_port = client_address
+        # Obter o IP e a porta de origem do cliente
         print('O clinte IP é:',client_ip)
         print('A porta do cliente é:',client_port)
 
@@ -76,76 +84,40 @@ def StartUDPServer(port, ip, matriz):
         print(Q)
         Y = str(parsed_data[3])
         print(Y)
-        P = str(parsed_data[4])
-        print(P)
-        NW = str(parsed_data[5])
-        print(NW)
-        NR = str(parsed_data[6])
-        print(NR)
-        #separa os dados da lista pelos pares 
-        W_data = parsed_data[7].split(';')
-        #volta a formar uma tupla de pares 
-        W = [(pair.split(',')[0], pair.split(',')[1]) for pair in W_data]
-        print(W)
-        R_data = parsed_data[8].split(';')
-        R = [(pair.split(',')[0], pair.split(',')[1]) for pair in R_data]
-        print(R)
+        filename = 'last_P.txt'
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                P, _ = map(float, file.readline().split())
+                print(f"P lido do arquivo: {P}")
 
-        if int(Y) == 2:
-            handle_set_primitive(data, client_address, matriz, UDPServerSocket)
-        else:
-                response = 'Your message was received, but it is not a set primitive.'
-                UDPServerSocket.sendto(response.encode(), client_address)
+        if int(Y)==1:
+            handle_get_primitiva(parsed_data)
 
-#NOTA: para implementar o que o prof pediu de uma thread por pedido, esta função ficaria assim:
-  #while True:
-   #     data, client_address = UDPServerSocket.recvfrom(4096)
-     #   client_thread = threading.Thread(target=handle_client_request, args=(data, client_address, matriz, UDPServerSocket))
-      #  client_thread.start()
+        elif int(Y) == 2:
+            handle_set_primitiva(parsed_data)
+            key, key_id = matriz.GenerateKey()
+            response = f"Generated Key: {key}, ID: {key_id}"
+            UDPServerSocket.sendto(response.encode(), client_address)
+            
+        elif int(Y)==3:
+            handle_response_primitiva(parsed_data)
+        
+        print(f"Thread para cliente {client_address} encerrada.")
 
-#def handle_client_request(data, client_address, matriz, UDPServerSocket):
-    #client_ip, client_port = client_address
-    #print('Cliente IP:', client_ip)
-    #print('Cliente Porta:', client_port)
-    # Tratamento dos dados(copy e paste do que esta na startupd server)
-    #Separa os dados pela terminação '\0' e descodifica os bytes
-    #parsed_data = data.decode().split('\0')
-    #print(parsed_data)
+#função que inicia o server udp, udp comm
+def StartUDPServer(port, ip, matriz):
+    localIP = ip
+    localPort = port
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    UDPServerSocket.bind((localIP, localPort))
+    print("UDP listening")
 
-    #acede ás posições dos dados
-    #S = parsed_data[0]
-    #print(S)
-    #NS = str(parsed_data[1])
-    #print(NS)
-    #Y = str(parsed_data[2])
-    #print(Y)
-    #P = str(parsed_data[3])
-    #print(P)
-    #NW = str(parsed_data[4])
-    #print(NW)
-    #NR = str(parsed_data[5])
-    #print(NR)
-    #separa os dados da lista pelos pares 
-    #W_data = parsed_data[6].split(';')
-    #volta a formar uma tupla de pares 
-    #W = [(pair.split(',')[0], pair.split(',')[1]) for pair in W_data]
-    #print(W)
-    #R_data = parsed_data[7].split(';')
-    #R = [(pair.split(',')[0], pair.split(',')[1]) for pair in R_data]
-    #print(R)
-
-    #if int(Y) == 2:
-     #   handle_set_primitive(data, client_address, matriz, UDPServerSocket)
-    #else:
-     #   print('outras primitivas')
-        #outras primitivas 
-
-
-def handle_set_primitive(data, client_address, matriz, UDPServerSocket):
-    print('Received a set primitive.')       
-    key, key_id = matriz.GenerateKey()
-    response = f"Generated Key: {key}, ID: {key_id}"
-    UDPServerSocket.sendto(response.encode(), client_address)
+    while True:
+        data, client_address = UDPServerSocket.recvfrom(4096)           
+        # Criar uma nova thread para lidar com o pedido do cliente
+        client_thread = threading.Thread(target=handle_client, args=(data, client_address, matriz, UDPServerSocket))
+        client_thread.start()
 
 def main():
     ip, port, m, k, t, v, x = ReadConfigFile()
@@ -161,4 +133,10 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
+       # https://cppsecrets.com/users/110711510497115104971101075756514864103109971051084699111109/Python-UDP-Server-with-Multiple-Clients.php
+        # codigo não funcional, é só um esboço
+        # c_thread = threading.Thread(target = self.handle_request, args = (data, client_address, matrizZ, MIB))
+        # c_thread.daemon = True
+        # c_thread.start()
+        # a função handle_request terá todo o código para tratar dos pedidos
